560f905ed417aa25e98d920707a5950b7c6a1af2
Shmuel Weinfeld <shmuelweinfeld@gmail.com>
2022-04-29 16:19:15 -0400

    final commit

--- src/components/GameComponent/GameComponent.js
+++ src/components/GameComponent/GameComponent.js
@@ -1,21 +1,38 @@
 import "./GameComponent.scss";
 
-function GameComponent() {
+function GameComponent(props) {
+    console.log(props.currentMovie.original_title)
+
+    function randomDate(start, end) {
+        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toISOString().slice(0,10);
+    }
+    
+    const handleCorrectAnswer = (e) => {
+        let clickedAnswer = e.target.innerHTML;
+        if(clickedAnswer == props.currentMovie.release_date) {
+            props.scoreIncrease();
+        } else {
+            props.indexIncrease();
+        }
+
+    }
+
     return(
         <section className="components">
+            <h2 className="components__title">{props.currentMovie.original_title}</h2>
             <img 
-                src="https://placedog.net/500"
+                src={`https://image.tmdb.org/t/p/w500/${props.currentMovie.backdrop_path}`}
                 className="components__image"
                 alt = "game"
             />
             <section className="components__buttons">
                 <div className="components__button">
-                    <button className="components__buttons--option" type="submit">SOMETHING</button>
-                    <button className="components__buttons--option" type="submit">SOMETHING</button>
+                    <button className="components__buttons--option" type="submit" onClick={handleCorrectAnswer}>{randomDate(new Date(2018, 0, 1), new Date())}</button>
+                    <button className="components__buttons--option" type="submit" onClick={handleCorrectAnswer}>{props.currentMovie.release_date}</button>
                 </div>
                 <div className="components__button">
-                    <button className="components__buttons--option" type="submit">SOMETHING</button>
-                    <button className="components__buttons--option" type="submit">SOMETHING</button>
+                    <button className="components__buttons--option" type="submit" onClick={handleCorrectAnswer}>{randomDate(new Date(2018, 0, 1), new Date())}</button>
+                    <button className="components__buttons--option" type="submit" onClick={handleCorrectAnswer}>{randomDate(new Date(2016, 0, 1), new Date())}</button>
                 </div>
             </section>
         </section>
--- src/pages/GamePage/GamePage.js
+++ src/pages/GamePage/GamePage.js
@@ -1,14 +1,86 @@
 import './GamePage.scss';
 import React from 'react';
 import GameComponent from "../../components/GameComponent/GameComponent";
+import axios from 'axios';
 
 class GamePage extends React.Component {
+    state = {
+        movieResults: null,
+        currentMovieQuestion: null,
+        score: 0,
+        index: 1
+    }
+    componentDidMount(){
+        axios
+          .get("https://api.themoviedb.org/3/movie/popular?api_key=4f5a995b5437bb04069689b2784c098c")
+          .then((response) => {
+            const movieResults = response.data.results;
+            let filteredResults = []
+            for(let i = 0; i<10; i++){
+                filteredResults.push(movieResults[i])
+            }
+            console.log('filtered res',filteredResults);
+            this.setState({movieResults: movieResults});
+            //set the default movie for trivia page
+            //let curIndex = 0;
+            let currentId = filteredResults[0].id;
+            return currentId;
+          })
+          .then((movieId) => {
+            this.fetchMovieDetails(movieId);
+          })
+      }
+
+      fetchMovieDetails = (movieId) => {
+        axios
+          .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=4f5a995b5437bb04069689b2784c098c`)
+          .then((response) => {
+            const movieData = response.data;
+            console.log('movie data',movieData)
+            this.setState({currentMovieQuestion: movieData});
+          })
+      }
+
+      increaseScore = () => {
+        //console.log(this.state.score);
+        console.log(this.state.index);
+        this.setState({
+          score: this.state.score + 1,
+          index: this.state.index + 1
+        });
+      } 
+      indexIncrease = () => {
+        this.setState({
+            //score: this.state.score + 1,
+            index: this.state.index + 1
+          });
+
+      }
+
+      componentDidUpdate(_prevProps, prevState) {
+        if(prevState.index !== this.state.index){
+            this.fetchMovieDetails(this.state.index);
+        } 
+      }
+
     render() {
         return(
+            
             <section className='main'>
-                <h2 className="main__heading">What year was this movie released?</h2>
-                <GameComponent />
+                {this.state.movieResults && this.state.currentMovieQuestion ? 
+                <>
+                    <h2 className="main__heading">What year was this movie released?</h2>
+                    <h2 className="main__score">{`Score: ${this.state.score}`}</h2>
+                    <GameComponent 
+                      currentMovie={this.state.currentMovieQuestion}
+                      score={this.state.score}
+                      scoreIncrease={this.increaseScore}
+                      indexIncrease={this.indexIncrease}
+                    />
+                </>
+                 : <p >Loading Content</p>}
             </section>
+            
         )
     }
 };
